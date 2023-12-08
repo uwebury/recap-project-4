ce3e0797d06f7722194a6a04f6de07b71412b9bb
uwebury <142432719+uwebury@users.noreply.github.com>
2023-12-08 11:29:14 +0100

    Added uid to activities

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Form from "./components/Form";
 import useLocalStorageState from "use-local-storage-state";
 import { useEffect, useState } from "react";
+import { uid } from "uid";
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
@@ -8,6 +9,8 @@ function App() {
   });
   function handleAddActivity(data) {
     console.log(data);
+    data.id = uid();
+    setActivities([...activities, data]);
   }
   return (
     <div className="App">
